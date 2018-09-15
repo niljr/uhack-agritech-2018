cfd3be9f703dd4d130cdb1eb8924745156f7309f
niljr <niljimenezjr@gmail.com>
2018-09-15 19:03:29 +0800

    tabs

--- src/components/Farmer.js
+++ src/components/Farmer.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from 'react';
+import { Tab } from 'semantic-ui-react'
 
-export default class Farmer extends PureComponent {
-    render() {
-        return (
-            <div>
-                Farmer
-            </div>
-        );
-    }
-}
+import FarmerDetails from './FarmerDetails';
+
+const panes = [
+    { menuItem: 'Farmer Details', render: () => <Tab.Pane attached={false}><FarmerDetails /></Tab.Pane> },
+    { menuItem: 'Sell Crops', render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane> },
+    { menuItem: 'Buy See', render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane> },
+  ]
+
+const Farmer = () => <Tab menu={{ pointing: true }} panes={panes} />
+
+  export default Farmer
